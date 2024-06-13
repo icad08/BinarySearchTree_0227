c42e15f2abd47c3944515a7ebde5a292facff2cf
icad08 <irsyadrm08@gmail.com>
2024-06-13 15:40:19 +0700

    add void search

--- binarytree.cpp
+++ binarytree.cpp
@@ -48,6 +48,14 @@ class BinaryTree {
             parent-> rightchild = newNode; //amke the right child of the parent point to the new node
         }
     }
+
+    void search(string, element, Node*& parent, Node*& currentNode){
+        currentNode = ROOT;
+        parent = NULL;
+        while ((currentNode != NULL) && (currentNode->info != element)){
+            
+        }
+    }
 };
 
 int main(){
