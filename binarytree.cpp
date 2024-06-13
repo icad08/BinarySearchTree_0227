2ace488268edfb758bb20a94bd228daf040fc9d3
icad08 <irsyadrm08@gmail.com>
2024-06-13 15:07:45 +0700

    add member of class node

--- binarytree.cpp
+++ binarytree.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
-class node{
+class Node{
+    public:
+    string info;
+    Node* leftchild;
+    Node* rightchild;
+
+    //constructor for the node class
     
 }
 
